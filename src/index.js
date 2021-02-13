cf639a7998aed6d8850a020ec0e1a544fc01ccad
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-02-13 21:23:28 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+    if (array !== undefined && array.length > 0) {
+        array = array.sort((a, b) => {
+            return a - b
+        })
+        return array[0]
+    } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array !== undefined && array.length > 0) {
+        array = array.sort((a, b) => {
+            return a - b
+        })
+        return array[array.length - 1]
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array !== undefined && array.length > 0) {
+        let sum = 0;
+
+        for (let i = 0; i < array.length; i++) {
+            sum = sum + array[i]
+
+
+
+
+        }
+
+        let average = sum / array.length
+        return average
+    } else return 0;
 }
